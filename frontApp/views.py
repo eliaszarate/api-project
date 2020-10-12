5f97dd1dc7cd285938610c4b8dbe85dc1e6013be
ezarate <ezarate@vmware.com>
2020-10-11 19:18:29 -0700

    third commit

--- frontApp/views.py
+++ frontApp/views.py
@@ -2,36 +2,35 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import os
 
-get_job_file = "./../src/myjobs.txt"
+get_job_file = "./src/myjobs.txt"
 
 file_count = open(get_job_file,"r")
 
-
+# Count the number of jobs
 numjobs = 0
 for i in file_count:
     numjobs += 1
 
 number = str(numjobs)
 
+
+# Home page ouput
 def index(request): 
-    return HttpResponse("Hello, Welcome to the front page, the web service has found " + number +  "possible positions for you to apply.\n"
+    return HttpResponse("Hello, Welcome to the front page, the web service has found " + number +  " possible positions for you to apply.\n"
            + "Please see /jobs for the title of the jobs and /jobs/links for the url links." )
-
-
-
 
 #file_content = open(./../scr/jobs.txt, 'r')
 
 #response = HttpResponse(targetfiles.read(), mimetime='text/plain') 
 def read_file(request):
-    f = open('./../src/myjobs.txt', 'r')
+    f = open('./src/myjobs.txt', 'r')
     file_content = f.read()
     f.close()
     return HttpResponse(file_content, content_type="text/plain")
 
 #response = HttpResponse(targetfiles.read(), mimetime='text/plain') 
 def read_links(request):
-    f = open('./../src/links.txt', 'r')
+    f = open('./src/links.txt', 'r')
     file_content = f.read()
     f.close()
     return HttpResponse(file_content, content_type="text/plain")
